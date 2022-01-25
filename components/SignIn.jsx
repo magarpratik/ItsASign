208b6105e117e85865e2b089041bd57256c0daff
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2022-01-25 09:44:02 +0000

    create basic setup of SignIn and SignUp pages

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,9 +1,14 @@
-import { Button } from "react-native";
-import { View, Text, StyleSheet } from "react-native";
+import { Button, Image, View, TextInput, StyleSheet } from "react-native";
 
 const SignIn = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://source.unsplash.com/random/sign language",
+        }}
+        style={styles.image}
+      />
       <View style={styles.button}>
         <Button
           color="green"
@@ -12,15 +17,26 @@ const SignIn = ({ navigation }) => {
         />
       </View>
 
-      <Text style={styles.message}>Hello, World!</Text>
+      <TextInput placeholder="Username" style={styles.textInput} />
+      <TextInput placeholder="Password" style={styles.textInput} />
+
+      <View style={styles.button}>
+        <Button color="green" title="Sign in" />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
-  message: {},
-  button: {},
+  container: { display: "flex", flexDirection: "column", alignItems: "center" },
+  button: { marginTop: 20 },
+  image: { width: 300, height: 300 },
+  textInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    width: 200,
+  },
 });
 
 export default SignIn;
--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -1,11 +1,28 @@
-import { Text, View } from "react-native";
+import { Button, Image, View, TextInput, StyleSheet } from "react-native";
 
 const SignUp = () => {
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={styles.container}>
+      <TextInput placeholder="Name" style={styles.textInput} />
+      <TextInput placeholder="Username" style={styles.textInput} />
+      <TextInput placeholder="Password" style={styles.textInput} />
+      <TextInput placeholder="Email" style={styles.textInput} />
+      <Button style={styles.button} color="green" title="Create account" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { display: "flex", flexDirection: "column", alignItems: "center" },
+  message: {},
+  button: {},
+  image: { width: 200, height: 200 },
+  textInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    width: 200,
+  },
+});
 
 export default SignUp;
