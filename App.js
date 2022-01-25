3f7e45b5ed419cc048dae62a68f71a774a3473eb
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2022-01-25 10:40:09 +0000

    navigate to Home Screen from Sign In Screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Home from "./components/Home";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +16,16 @@ export default function App() {
           component={SignIn}
           options={{ title: "Sign in Screen" }}
         />
-        <Stack.Screen name="SignUp" component={SignUp} options={{ title: "Sign up Screen" }}/>
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ title: "Sign up Screen" }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Home Screen" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
