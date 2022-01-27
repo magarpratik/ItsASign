957cc42b0ffb2a01ce95807dfa1219d534c8ed1e
Josh-Human <jhumanb1@gmail.com>
2022-01-27 11:19:22 +0000

    dynamic level, progress bar

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,36 +1,52 @@
+import { useEffect, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { Bar } from "react-native-progress";
 
 const Profile = () => {
-  return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri: "https://api.multiavatar.com/helloworld.png",
-        }}
-        style={styles.image}
-      />
-      <View style={styles.bar}>
-        <Bar progress={0.3} width={250} height={20} />
-        <Text>Level 1</Text>
-      </View>
-    </View>
-  );
+    const [level, setLevel] = useState(0);
+    const [progress, setProgress] = useState(0);
+    const [totalXP, setTotalXP] = useState(0);
+    useEffect(() => {
+        setTotalXP(153);
+    }, []);
+    useEffect(() => {
+        setLevel(Math.floor(totalXP / 100));
+        setProgress((totalXP % 100) / 100);
+    }, [totalXP]);
+
+    // request total xp at some point
+    // calculate from there
+    console.log(level);
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{
+                    uri: "https://api.multiavatar.com/helloworld.png",
+                }}
+                style={styles.image}
+            />
+            <Text>username</Text>
+            <View style={styles.bar}>
+                <Bar progress={progress} width={250} height={20} />
+                <Text>Level {level}</Text>
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  image: { width: 300, height: 300 },
-  bar: {
-    marginHorizontal: 10,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    image: { width: 300, height: 300 },
+    bar: {
+        marginHorizontal: 10,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
 });
 
 export default Profile;
