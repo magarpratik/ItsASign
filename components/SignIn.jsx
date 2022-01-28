37546151a6b6575a069ae80820a420e0cbe609ed
Josh-Human <jhumanb1@gmail.com>
2022-01-28 12:27:04 +0000

    use context for signing in

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,46 +1,71 @@
+import { useState, useContext } from "react";
+
 import { Button, Image, View, TextInput, StyleSheet } from "react-native";
+import { UserContext } from "../utils/userContext";
 
 const SignIn = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri: "https://source.unsplash.com/random/sign language",
-        }}
-        style={styles.image}
-      />
-      <View style={styles.button}>
-        <Button
-          color="green"
-          title="Sign up!"
-          onPress={() => navigation.navigate("SignUp", { name: "Jane" })}
-        />
-      </View>
+    const [userText, setUserText] = useState("");
+    const [passwordText, setPasswordText] = useState("");
 
-      <TextInput placeholder="Username" style={styles.textInput} />
-      <TextInput placeholder="Password" style={styles.textInput} />
+    const { username, setUsername } = useContext(UserContext);
 
-      <View style={styles.button}>
-        <Button
-          color="green"
-          title="Sign in"
-          onPress={() => navigation.navigate("HomePage")}
-        />
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{
+                    uri: "https://source.unsplash.com/random/sign language",
+                }}
+                style={styles.image}
+            />
+            <View style={styles.button}>
+                <Button
+                    color="green"
+                    title="Sign up!"
+                    onPress={() =>
+                        navigation.navigate("SignUp", { name: "Jane" })
+                    }
+                />
+            </View>
+
+            <TextInput
+                placeholder="Username"
+                style={styles.textInput}
+                onChangeText={setUserText}
+            />
+            <TextInput
+                placeholder="Password"
+                style={styles.textInput}
+                onChangeText={setPasswordText}
+            />
+
+            <View style={styles.button}>
+                <Button
+                    color="green"
+                    title="Sign in"
+                    onPress={() => {
+                        setUsername(userText);
+                        navigation.navigate("HomePage");
+                    }}
+                />
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: { display: "flex", flexDirection: "column", alignItems: "center" },
-  button: { marginTop: 20 },
-  image: { width: 300, height: 300 },
-  textInput: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    width: 200,
-  },
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    button: { marginTop: 20 },
+    image: { width: 300, height: 300 },
+    textInput: {
+        height: 40,
+        borderColor: "gray",
+        borderWidth: 1,
+        width: 200,
+    },
 });
 
 export default SignIn;
