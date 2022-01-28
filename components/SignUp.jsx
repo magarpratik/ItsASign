dcd4f6bb727a4e02f74bb0301037aed65158c6b3
Josh-Human <jhumanb1@gmail.com>
2022-01-28 14:45:42 +0000

    current text and submit states created

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import { Button, Image, View, TextInput, StyleSheet } from "react-native";
+import { set } from "react-native-reanimated";
 
 const SignUp = ({ navigation }) => {
+    const [name, setName] = useState("");
+    // just as username would conflict with context?
+    const [usernameSignUp, setUsernameSignUp] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    const [nameText, setNameText] = useState("");
+    const [usernameSignUpText, setUsernameSignUpText] = useState("");
+    const [passwordText, setPasswordText] = useState("");
+    const [emailText, setEmailText] = useState("");
+
+    const handleSignUp = () => {
+        setName(nameText);
+        setUsernameSignUp(usernameSignUpText);
+        setPassword(passwordText);
+        setEmail(emailText);
+        navigation.navigate("SignIn");
+    };
+
     return (
         <View style={styles.container}>
             <TextInput placeholder="Name" style={styles.textInput} />
@@ -15,9 +36,7 @@ const SignUp = ({ navigation }) => {
                 style={styles.button}
                 color="green"
                 title="Create account"
-                onPress={() => {
-                    navigation.navigate("SignIn");
-                }}
+                onPress={handleSignUp}
             />
         </View>
     );
