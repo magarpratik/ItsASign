e085b14ff052ac22f7b4196689a01d188cb39fd5
Josh-Human <jhumanb1@gmail.com>
2022-01-28 15:27:51 +0000

    validation working

--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -14,29 +14,78 @@ const SignUp = ({ navigation }) => {
     const [passwordText, setPasswordText] = useState("");
     const [emailText, setEmailText] = useState("");
 
+    const [isValidUsername, setIsValidUsername] = useState(true);
+    const [isValidPassword, setIsValidPassword] = useState(true);
+    const [isValidEmail, setIsValidEmail] = useState(true);
+
     const handleSignUp = () => {
+        const checkPasswordValid = (text) => {
+            return text.match(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{8,50})/) !== null;
+        };
+        const checkEmailValid = (text) => {
+            return (
+                text.match(
+                    /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+                ) !== null
+            );
+        };
+
         setName(nameText);
         setUsernameSignUp(usernameSignUpText);
         setPassword(passwordText);
         setEmail(emailText);
-        navigation.navigate("SignIn");
+
+        if (usernameSignUpText.length < 4) {
+            console.log("user");
+            setIsValidUsername(false);
+        } else if (!checkPasswordValid(passwordText)) {
+            console.log("pass");
+            setIsValidPassword(false);
+        } else if (!checkEmailValid(emailText)) {
+            console.log(emailText);
+            setIsValidEmail(false);
+        } else {
+            console.log("navv");
+            navigation.navigate("SignIn");
+        }
+
+        // send api request to make user
     };
 
     return (
         <View style={styles.container}>
-            <TextInput placeholder="Name" style={styles.textInput} />
-            <TextInput placeholder="Username" style={styles.textInput} />
+            <TextInput
+                placeholder="Name"
+                style={styles.textInput}
+                onChangeText={setNameText}
+            />
+            {/* {isValidUsername ? null : (
+                <Text>
+                    Invalid Username! Should be longer than 4 characters.
+                </Text>
+            )} */}
+            <TextInput
+                placeholder="Username"
+                style={styles.textInput}
+                onChangeText={setUsernameSignUpText}
+            />
             <TextInput
                 placeholder="Password"
                 style={styles.textInput}
                 secureTextEntry={true}
+                onChangeText={setPasswordText}
             />
-            <TextInput placeholder="Email" style={styles.textInput} />
+            <TextInput
+                placeholder="Email"
+                style={styles.textInput}
+                onChangeText={setEmailText}
+            />
             <Button
                 style={styles.button}
                 color="green"
                 title="Create account"
                 onPress={handleSignUp}
+                onChangeText={setPasswordText}
             />
         </View>
     );
