bd0d23b11b86f24e8067735c04a6c477cd075015
AndyRobotsNC <andyrobotsdroid@gmail.com>
2022-01-31 12:50:07 +0000

    changed

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,73 +1,83 @@
 import { useState, useContext } from "react";
 
-import { Button, Image, View, TextInput, StyleSheet } from "react-native";
+import { Button, Image, View, TextInput, StyleSheet, Text } from "react-native";
 import { UserContext } from "../utils/userContext";
 
 const SignIn = ({ navigation }) => {
-    const [userText, setUserText] = useState("");
-    const [passwordText, setPasswordText] = useState("");
+  const [userText, setUserText] = useState("");
+  const [passwordText, setPasswordText] = useState("");
 
-    const { username, setUsername } = useContext(UserContext);
+  const { username, setUsername } = useContext(UserContext);
 
-    return (
-        <View style={styles.container}>
-            <Image
-                source={{
-                    uri: "https://source.unsplash.com/random/sign language",
-                }}
-                style={styles.image}
-            />
-            <View style={styles.button}>
-                <Button
-                    color="green"
-                    title="Sign up!"
-                    onPress={() =>
-                        navigation.navigate("SignUp", { name: "Jane" })
-                    }
-                />
-            </View>
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://source.unsplash.com/random/sign language",
+        }}
+        style={styles.image}
+      />
 
-            <TextInput
-                placeholder="Username"
-                style={styles.textInput}
-                onChangeText={setUserText}
-            />
-            <TextInput
-                placeholder="Password"
-                style={styles.textInput}
-                onChangeText={setPasswordText}
-                secureTextEntry={true}
-            />
+      <TextInput
+        placeholder="Username"
+        style={styles.textInput}
+        onChangeText={setUserText}
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.textInput}
+        onChangeText={setPasswordText}
+        secureTextEntry={true}
+      />
 
-            <View style={styles.button}>
-                <Button
-                    color="green"
-                    title="Sign in"
-                    onPress={() => {
-                        setUsername(userText);
+      <View style={styles.button}>
+        <Button
+          color="white"
+          title="Sign in"
+          onPress={() => {
+            setUsername(userText);
 
-                        navigation.navigate("HomePage");
-                    }}
-                />
-            </View>
-        </View>
-    );
+            navigation.navigate("HomePage");
+          }}
+        />
+      </View>
+      <Text>Haven't got an account yet?</Text>
+      <View style={styles.button}>
+        <Button
+          color="white"
+          title="Sign up!"
+          onPress={() => navigation.navigate("SignUp", { name: "Jane" })}
+        />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    button: { marginTop: 20 },
-    image: { width: 300, height: 300 },
-    textInput: {
-        height: 40,
-        borderColor: "gray",
-        borderWidth: 1,
-        width: 200,
-    },
+  container: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#78ba97",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#56a996",
+    margin: 20,
+    borderRadius: 10,
+    width: 150,
+  },
+  image: { width: 300, height: 300 },
+  textInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    width: 200,
+    padding: 5,
+    margin: 5,
+    backgroundColor: "white",
+  },
 });
 
 export default SignIn;
--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -1,43 +1,54 @@
 import { Button, Image, View, TextInput, StyleSheet } from "react-native";
 
 const SignUp = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <TextInput placeholder="Name" style={styles.textInput} />
-            <TextInput placeholder="Username" style={styles.textInput} />
-            <TextInput
-                placeholder="Password"
-                style={styles.textInput}
-                secureTextEntry={true}
-            />
-            <TextInput placeholder="Email" style={styles.textInput} />
-            <Button
-                style={styles.button}
-                color="green"
-                title="Create account"
-                onPress={() => {
-                    navigation.navigate("SignIn");
-                }}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder="Name" style={styles.textInput} />
+      <TextInput placeholder="Username" style={styles.textInput} />
+      <TextInput
+        placeholder="Password"
+        style={styles.textInput}
+        secureTextEntry={true}
+      />
+      <TextInput placeholder="Email" style={styles.textInput} />
+      <View style={styles.button}>
+        <Button
+          color="white"
+          title="Create account"
+          onPress={() => {
+            navigation.navigate("SignIn");
+          }}
+        />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    message: {},
-    button: {},
-    image: { width: 200, height: 200 },
-    textInput: {
-        height: 40,
-        borderColor: "gray",
-        borderWidth: 1,
-        width: 200,
-    },
+  container: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#78ba97",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#56a996",
+    margin: 10,
+    borderRadius: 10,
+    width: 150,
+  },
+  image: { width: 200, height: 200 },
+  textInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    width: 200,
+    padding: 5,
+    margin: 5,
+    backgroundColor: "white",
+  },
 });
 
 export default SignUp;
