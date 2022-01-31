2c8ec288ca6f66544345875f85c05689ac83c6b9
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2022-01-31 11:34:03 +0000

    get lessons for a course + store them in a state in Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,116 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-    FlatList,
-    Image,
-    Pressable,
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
+  FlatList,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
 } from "react-native";
+import { getLessons } from "../utils/api";
 import { UserContext } from "../utils/userContext";
 import Badges from "./Badges";
 
 const Home = ({ navigation: { navigate } }) => {
-    // const { username } = React.useContext(UserContext);
-    // console.log(username);
-    const lessons = [
-        {
-            id: "1",
-            text: "Lesson 1",
-            locked: false,
-        },
-        {
-            id: "2",
-            text: "Lesson 2",
-            locked: false,
-        },
-        {
-            id: "3",
-            text: "Lesson 3",
-            locked: true,
-        },
-        {
-            id: "4",
-            text: "Lesson 4",
-            locked: true,
-        },
-        {
-            id: "5",
-            text: "Lesson 5",
-            locked: true,
-        },
-    ];
+  // const { username } = React.useContext(UserContext);
+  // console.log(username);
 
-    const Item = ({ title, id, locked }) => (
-        <Pressable
-            onPress={() => {
-                console.log(title);
-                console.log(id);
-                navigate("Lesson");
-            }}
-            disabled={locked}
-            style={({ pressed }) => [
-                {
-                    id: "1",
-                    text: "Lesson 1",
-                    locked: false,
-                },
-                styles.wrapperCustom,
-            ]}
-        >
-            <View
-                style={{
-                    backgroundColor: locked ? "grey" : "#5cb85c",
-                    padding: 20,
-                    marginVertical: 8,
-                    marginHorizontal: 16,
-                    borderRadius: 15,
-                }}
-            >
-                <Text style={styles.lessonText}>{title}</Text>
-            </View>
-        </Pressable>
-    );
+  const [testLessons, setTestLessons] = useState([{ course_topic: "test" }]);
 
-    const renderItem = ({ item }) => (
-        <Item title={item.text} id={item.id} locked={item.locked} />
-    );
+  useEffect(() => {
+    getLessons().then((lessons) => setTestLessons(lessons));
+    console.log(testLessons[0].course_topic);
+  }, [testLessons[0].course_topic]);
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.header}>Available lessons</Text>
-            <FlatList
-                data={lessons}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id}
-                style={{
-                    height: "60%",
-                    backgroundColor: "eggshell",
-                    flexGrow: 0,
-                }}
-            />
-            <Badges></Badges>
-        </View>
-    );
+  const lessons = [
+    {
+      id: "1",
+      text: "Lesson 1",
+      locked: false,
+    },
+    {
+      id: "2",
+      text: "Lesson 2",
+      locked: false,
+    },
+    {
+      id: "3",
+      text: "Lesson 3",
+      locked: true,
+    },
+    {
+      id: "4",
+      text: "Lesson 4",
+      locked: true,
+    },
+    {
+      id: "5",
+      text: "Lesson 5",
+      locked: true,
+    },
+  ];
+
+  const Item = ({ title, id, locked }) => (
+    <Pressable
+      onPress={() => {
+        console.log(title);
+        console.log(id);
+        navigate("Lesson");
+      }}
+      disabled={locked}
+      style={({ pressed }) => [
+        {
+          id: "1",
+          text: "Lesson 1",
+          locked: false,
+        },
+        styles.wrapperCustom,
+      ]}
+    >
+      <View
+        style={{
+          backgroundColor: locked ? "grey" : "#5cb85c",
+          padding: 20,
+          marginVertical: 8,
+          marginHorizontal: 16,
+          borderRadius: 15,
+        }}
+      >
+        <Text style={styles.lessonText}>{title}</Text>
+      </View>
+    </Pressable>
+  );
+
+  const renderItem = ({ item }) => (
+    <Item title={item.text} id={item.id} locked={item.locked} />
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Available lessons</Text>
+      <FlatList
+        data={lessons}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        style={{
+          height: "60%",
+          backgroundColor: "eggshell",
+          flexGrow: 0,
+        }}
+      />
+      <Badges></Badges>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-    },
-    lessonText: {
-        fontSize: 24,
-    },
-    header: {
-        fontSize: 32,
-        paddingTop: 15,
-        paddingBottom: 15,
-        textAlign: "center",
-        marginBottom: 0,
-    },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  lessonText: {
+    fontSize: 24,
+  },
+  header: {
+    fontSize: 32,
+    paddingTop: 15,
+    paddingBottom: 15,
+    textAlign: "center",
+    marginBottom: 0,
+  },
 });
 
 export default Home;
--- utils/api.js
+++ utils/api.js
@@ -1,23 +1,34 @@
 import axios from "axios";
 
 const signApi = axios.create({
-    baseURL: "https://its-a-sign-app.herokuapp.com/",
+  baseURL: "https://its-a-sign-app.herokuapp.com/",
 });
 
 export const postUser = (name, username, password, email) => {
-    console.log("here");
-    return signApi
-        .post("/api/users/signup", {
-            username: username,
-            email: email,
-            password: password,
-            confirmPassword: password,
-        })
-        .then((response) => {
-            console.log(response);
-            return response.data.message;
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+  console.log("here");
+  return signApi
+    .post("/api/users/signup", {
+      username: username,
+      email: email,
+      password: password,
+      confirmPassword: password,
+    })
+    .then((response) => {
+      console.log(response);
+      return response.data.message;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const getLessons = (course_topic) => {
+  return signApi
+    .get("/api/courses/alpha")
+    .then((response) => {
+      return response.data.courses;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
