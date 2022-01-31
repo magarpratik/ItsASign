ea3956562878a9581a9091146e703eedb1230a8a
AndyRobotsNC <andyrobotsdroid@gmail.com>
2022-01-31 15:38:03 +0000

    home page button

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,77 +1,76 @@
 import { useState, useContext, useEffect } from "react";
 
 import {
-    Button,
-    Image,
-    View,
-    TextInput,
-    StyleSheet,
-    Text,
-    Pressable,
+  Button,
+  Image,
+  View,
+  TextInput,
+  StyleSheet,
+  Text,
+  Pressable,
 } from "react-native";
 import { getUser } from "../utils/api";
 import { UserContext } from "../utils/userContext";
 
 const SignIn = ({ navigation }) => {
-    const [userText, setUserText] = useState("");
-    const [passwordText, setPasswordText] = useState("");
-    const [password, setPassword] = useState("");
+  const [userText, setUserText] = useState("");
+  const [passwordText, setPasswordText] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [isValidUsername, setIsValidUsername] = useState(true);
-    const [isValidPassword, setIsValidPassword] = useState(true);
-    const [isFirstLoad, setIsFirstLoad] = useState(true);
-
+  const [isValidUsername, setIsValidUsername] = useState(true);
+  const [isValidPassword, setIsValidPassword] = useState(true);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
 
   const { username, setUsername } = useContext(UserContext);
 
-    const handleSignIn = () => {
-        setIsValidPassword(true);
-        setIsValidUsername(true);
-        const checkPasswordValid = (text) => {
-            return text.match(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{8,50})/) !== null;
-        };
-
-        setUsername(userText);
-        setPassword(passwordText);
-
-        if (userText.length < 4) {
-            setIsValidUsername(false);
-        } else if (!checkPasswordValid(passwordText)) {
-            setIsValidPassword(false);
-        }
-
-        // send api request to make user
+  const handleSignIn = () => {
+    setIsValidPassword(true);
+    setIsValidUsername(true);
+    const checkPasswordValid = (text) => {
+      return text.match(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{8,50})/) !== null;
     };
 
-    useEffect(() => {
-        if (isFirstLoad) {
-            setIsFirstLoad(false);
-        } else {
-            getUser(username).then((result) => {
-                if (result) {
-                    // if backend is string
-                    if (result.password === password) {
-                        navigation.navigate("HomePage");
-                    } else {
-                        setIsValidPassword(false);
-                        console.log(result.message);
-                    }
-                } else {
-                    setIsValidUsername(false);
-                }
-            });
-        }
-    }, [username, password]);
+    setUsername(userText);
+    setPassword(passwordText);
 
-    return (
-        <View style={styles.container}>
-            <Image
-                source={{
-                    uri: "https://source.unsplash.com/random/sign language",
-                }}
-                style={styles.image}
-            />
-            {/* <View style={styles.button}>
+    if (userText.length < 4) {
+      setIsValidUsername(false);
+    } else if (!checkPasswordValid(passwordText)) {
+      setIsValidPassword(false);
+    }
+
+    // send api request to make user
+  };
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      setIsFirstLoad(false);
+    } else {
+      getUser(username).then((result) => {
+        if (result) {
+          // if backend is string
+          if (result.password === password) {
+            navigation.navigate("HomePage");
+          } else {
+            setIsValidPassword(false);
+            console.log(result.message);
+          }
+        } else {
+          setIsValidUsername(false);
+        }
+      });
+    }
+  }, [username, password]);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://source.unsplash.com/random/sign language",
+        }}
+        style={styles.image}
+      />
+      {/* <View style={styles.button}>
                 <Button
                     color="green"
                     title="Sign up!"
@@ -80,54 +79,60 @@ const SignIn = ({ navigation }) => {
                     }
                 />
             </View> */}
-            <Pressable
-                onPress={() => navigation.navigate("SignUp", { name: "Jane" })}
-                style={styles.button}
-            >
-                <Text style={styles.buttonText}>Sign up!</Text>
-            </Pressable>
+      <Pressable
+        onPress={() => navigation.navigate("SignUp", { name: "Jane" })}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign up!</Text>
+      </Pressable>
 
-            <TextInput
-                placeholder="Username"
-                style={styles.textInput}
-                onChangeText={setUserText}
-            />
-            {isValidUsername ? null : (
-                <Text style={styles.errorText}>User does not exist.</Text>
-            )}
-            <TextInput
-                placeholder="Password"
-                style={styles.textInput}
-                onChangeText={setPasswordText}
-                secureTextEntry={true}
-            />
-            {isValidPassword ? null : (
-                <Text style={styles.errorText}>Incorrect password.</Text>
-            )}
+      <TextInput
+        placeholder="Username"
+        style={styles.textInput}
+        onChangeText={setUserText}
+      />
+      {isValidUsername ? null : (
+        <Text style={styles.errorText}>User does not exist.</Text>
+      )}
+      <TextInput
+        placeholder="Password"
+        style={styles.textInput}
+        onChangeText={setPasswordText}
+        secureTextEntry={true}
+      />
+      {isValidPassword ? null : (
+        <Text style={styles.errorText}>Incorrect password.</Text>
+      )}
 
-            <Pressable style={styles.button} onPress={handleSignIn}>
-                <Text style={styles.buttonText}>Sign in</Text>
-            </Pressable>
-        </View>
-    );
+      <Pressable style={styles.button} onPress={handleSignIn}>
+        <Text style={styles.buttonText}>Sign in</Text>
+      </Pressable>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate("HomePage", { name: "Jane" })}
+      >
+        <Text style={styles.buttonText}>Home page</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
     display: "flex",
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
     backgroundColor: "#78ba97",
   },
-    button: {
+  button: {
     marginTop: 20,
     backgroundColor: "#56a996",
     margin: 20,
     borderRadius: 10,
     width: 150,
   },
-    
+
   image: { width: 300, height: 300 },
   textInput: {
     height: 40,
@@ -138,13 +143,13 @@ const styles = StyleSheet.create({
     margin: 5,
     backgroundColor: "white",
   },
-    buttonText: {
-        color: "white",
-    },
-    errorText: {
-        color: "red",
-        width: 200,
-    },
+  buttonText: {
+    color: "white",
+  },
+  errorText: {
+    color: "red",
+    width: 200,
+  },
 });
 
 export default SignIn;
