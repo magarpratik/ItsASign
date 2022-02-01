a554efbf6a9174682c772f6837b09233b5b3a7cb
AndyRobotsNC <andyrobotsdroid@gmail.com>
2022-02-01 11:59:05 +0000

    badges and pressable

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -6,51 +6,53 @@ import Badges from "./Badges";
 import React from "react";
 
 const Profile = () => {
-    const { username } = React.useContext(UserContext);
-    const [level, setLevel] = useState(0);
-    const [progress, setProgress] = useState(0);
-    const [totalXP, setTotalXP] = useState(0);
+  const { username } = React.useContext(UserContext);
+  const [level, setLevel] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const [totalXP, setTotalXP] = useState(0);
 
-    useEffect(() => {
-        setTotalXP(153);
-    }, []);
-    useEffect(() => {
-        setLevel(Math.floor(totalXP / 100));
-        setProgress((totalXP % 100) / 100);
-    }, [totalXP]);
+  useEffect(() => {
+    setTotalXP(153);
+  }, []);
+  useEffect(() => {
+    setLevel(Math.floor(totalXP / 100));
+    setProgress((totalXP % 100) / 100);
+  }, [totalXP]);
 
-    return (
-        <View style={styles.container}>
-            <Image
-                source={{
-                    uri: "https://api.multiavatar.com/helloworld.png",
-                }}
-                style={styles.image}
-            />
-            <Text>{username}</Text>
-            <View style={styles.bar}>
-                <Bar progress={progress} width={250} height={20} />
-                <Text>Level {level}</Text>
-            </View>
-            <Badges></Badges>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://api.multiavatar.com/helloworld.png",
+        }}
+        style={styles.image}
+      />
+      <Text>{username}</Text>
+      <View style={styles.bar}>
+        <Bar progress={progress} width={250} height={20} />
+        <Text>
+          Level {level} - {progress * 100}/100
+        </Text>
+      </View>
+      <Badges totalXP={totalXP} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    image: { width: 300, height: 300, marginTop: 30, marginBottom: 100 },
-    bar: {
-        marginHorizontal: 10,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: 10,
-    },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  image: { width: 300, height: 300, marginTop: 30, marginBottom: 100 },
+  bar: {
+    marginHorizontal: 10,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 10,
+  },
 });
 
 export default Profile;
