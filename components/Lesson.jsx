3aeb259c5b2e513b70a174f8a66177b7298c7c3e
AndyRobotsNC <andyrobotsdroid@gmail.com>
2022-02-02 11:50:16 +0000

    first lesson page styling

--- components/Lesson.jsx
+++ components/Lesson.jsx
@@ -1,22 +1,71 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View, Touchable, TouchableOpacity, Image } from 'react-native';
-
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Touchable,
+  TouchableOpacity,
+  Image,
+  Pressable,
+} from "react-native";
 
 const Lesson = ({ setLessonCompleted }) => {
+  const completeLesson = () => {
+    setLessonCompleted(true);
+  };
 
-    const completeLesson = () => {
-        setLessonCompleted(true)
-    }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>
+        Today you will be learning how to sign the letter 'A'
+      </Text>
+      <Image style={styles.img} source={require("../assets/A.png")} />
+      <Pressable style={styles.button} onPress={completeLesson}>
+        <Text style={styles.buttonText}>Start</Text>
+      </Pressable>
+    </View>
+  );
+};
 
-    return (
-        <View>
-            <Text>Today you will be learning how to sign the letter 'A'.</Text>
-            <Image style={{width:50, height:50}} source={require('../assets/A.png')} />
-            <Button title="Next" onPress={completeLesson} />
-        </View>
-    )
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    marginTop: 30,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#F0F7F4",
+  },
+  header: {
+    fontSize: 32,
+    paddingTop: 15,
+    paddingBottom: 15,
+    textAlign: "center",
+    marginBottom: 0,
+  },
+  img: {
+    width: 250,
+    height: 250,
+    borderRadius: 100 / 2,
+    overflow: "hidden",
+    borderWidth: 3,
+    borderColor: "grey",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#004346",
+    margin: 20,
+    borderRadius: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    height: 50,
+    width: 100,
+  },
+  buttonText: {
+    color: "white",
+    alignSelf: "center",
+    fontSize: 30,
+  },
+});
 
-
-}
-
-export { Lesson }
+export { Lesson };
