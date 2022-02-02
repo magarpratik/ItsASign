25d7e2f2ad3b3399c4a03ca12aa159fffe69e6e5
AndyRobotsNC <andyrobotsdroid@gmail.com>
2022-02-02 10:22:37 +0000

    profile loading wheel

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useState, useContext } from "react";
 import { Image, StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { Bar } from "react-native-progress";
-import { UserContext } from "../utils/userContext";
+import { UserContext, LoadingContext } from "../utils/userContext";
 import Badges from "./Badges";
 import React from "react";
 
 const Profile = () => {
   const { username } = React.useContext(UserContext);
+  const { isLoading, setIsLoading } = React.useContext(LoadingContext);
   const [level, setLevel] = useState(0);
   const [progress, setProgress] = useState(0);
   const [totalXP, setTotalXP] = useState(0);
 
   useEffect(() => {
+    setIsLoading(true);
     setTotalXP(153);
+    setIsLoading(false);
   }, []);
   useEffect(() => {
     setLevel(Math.floor(totalXP / 100));
@@ -20,21 +23,27 @@ const Profile = () => {
   }, [totalXP]);
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri: "https://api.multiavatar.com/helloworld.png",
-        }}
-        style={styles.image}
-      />
-      <Text>{username}</Text>
-      <View style={styles.bar}>
-        <Bar progress={progress} width={250} height={20} />
-        <Text>
-          Level {level} - {progress * 100}/100
-        </Text>
-      </View>
-      <Badges totalXP={totalXP} />
+    <View>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#00ff00" />
+      ) : (
+        <View style={styles.container}>
+          <Image
+            source={{
+              uri: "https://api.multiavatar.com/helloworld.png",
+            }}
+            style={styles.image}
+          />
+          <Text>{username}</Text>
+          <View style={styles.bar}>
+            <Bar progress={progress} width={250} height={20} />
+            <Text>
+              Level {level} - {progress * 100}/100
+            </Text>
+          </View>
+          <Badges totalXP={totalXP} />
+        </View>
+      )}
     </View>
   );
 };
